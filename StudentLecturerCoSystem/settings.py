6f7945c85092ad7ddb72d17aecd397729ecd9e93
kyagieboy <kyagieboy99@gmail.com>
2020-09-16 13:20:22 +0300

    Database changed to mysql

--- StudentLecturerCoSystem/settings.py
+++ StudentLecturerCoSystem/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'StudentLecturerCoSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stdlec',
+        'USER': 'kyagie',
+        'PASSWORD': 'greenhill123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
     }
 }
 
