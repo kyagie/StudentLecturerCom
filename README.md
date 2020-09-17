e612b73f0ceb8783df55270939080b20fb482631
Arthur Kyagulanyi <akyagulanyi@tayariafrica.com>
2020-09-17 19:04:51 +0300

    Added signup page

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Resources
 https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Authentication
 
 https://learndjango.com/tutorials/django-signup-tutorial
+https://techwithtim.net/tutorials/django/user-registration/
--- StudentLecturerCoSystem/urls.py
+++ StudentLecturerCoSystem/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from stdlec import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("register/", views.register, name="register"),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
