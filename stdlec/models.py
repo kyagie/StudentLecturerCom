aff790860fc141345dfe7ae86f52ac7e7f83f8a6
Arthur Kyagulanyi <akyagulanyi@tayariafrica.com>
2020-09-28 08:31:45 +0300

    Validates

--- stdlec/models.py
+++ stdlec/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+
 
 # Create your models here.
 class Student(models.Model):
     sfname = models.CharField(max_length=50, blank=False)
     slname = models.CharField(max_length=50, blank=False)
-    semail = models.EmailField(blank=False, unique=True)
+    semail = models.EmailField(blank=False, unique=True, validators=[validate_email])
     sregno = models.CharField(max_length=50, blank=False, unique=True)
     scourse = models.CharField(max_length=50, blank=False)
     scontact = models.IntegerField(blank=False)
